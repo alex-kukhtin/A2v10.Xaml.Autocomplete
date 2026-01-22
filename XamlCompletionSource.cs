753e5281949862b21d99fc42eb44eafdd0e09dd5
alex-kukhtin <alex.kukhtin@outlook.com>
2026-01-22 15:15:20 +0200

    Add project files.

---
+++ XamlCompletionSource.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © 2021-2024 Oleksandr Kukhtin. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using Microsoft.VisualStudio.Text;
+
+namespace A2v10XamlAutocomplete;
+
+internal class XamlCompletionSource : IAsyncCompletionSource
+{
+    public Task<CompletionContext> GetCompletionContextAsync(IAsyncCompletionSession session, CompletionTrigger trigger, SnapshotPoint triggerLocation, SnapshotSpan applicableToSpan, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<object> GetDescriptionAsync(IAsyncCompletionSession session, CompletionItem item, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CompletionStartData InitializeCompletion(CompletionTrigger trigger, SnapshotPoint triggerLocation, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ XamlCompletionSourceProvider.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2026 Oleksandr Kukhtin. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace A2v10XamlAutocomplete;
+
+[Export(typeof(IAsyncCompletionSourceProvider))]
+[Name("A2v10 XAML completion provider")]
+[ContentType("xml")]
+internal class XamlCompletionSourceProvider : IAsyncCompletionSourceProvider
+{
+    public IAsyncCompletionSource GetOrCreate(ITextView textView)
+    {
+        throw new NotImplementedException();
+    }
+}
