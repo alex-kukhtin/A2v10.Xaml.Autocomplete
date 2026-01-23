436571867cc7547651e5585a48c3a2662d11e1dd
alex-kukhtin <alex.kukhtin@outlook.com>
2026-01-23 16:52:30 +0200

    completion source

--- XamlCompletionSource.cs
+++ XamlCompletionSource.cs
@@ -1,20 +1,29 @@
 ﻿// Copyright © 2021-2024 Oleksandr Kukhtin. All rights reserved.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
+using Microsoft.VisualStudio.Core.Imaging;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Adornments;
+using Microsoft.VisualStudio.Text.Operations;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace A2v10XamlAutocomplete;
 
-internal class XamlCompletionSource : IAsyncCompletionSource
+internal class XamlCompletionSource(ITextStructureNavigatorSelectorService _structureNavigatorSelector) : IAsyncCompletionSource
 {
+
+    static Guid ImageLibraryGuid = new Guid("ae27a6b0-e345-4288-96df-5eaf394ee369");
+
+    static ImageElement TagIcon = new ImageElement(new ImageId(ImageLibraryGuid, 1873), "Tag");
+    static ImageElement PropertyIcon = new ImageElement(new ImageId(ImageLibraryGuid, 2429), "Property");
+    static ImageElement EnumIcon = new ImageElement(new ImageId(ImageLibraryGuid, 1120), "Enum");
+    static ImageElement XmlCommentIcon = new ImageElement(new ImageId(ImageLibraryGuid, 3568), "XmlComment");
+    static ImageElement XmlCDataIcon = new ImageElement(new ImageId(ImageLibraryGuid, 3567), "XmlCData");
+
+
     public Task<CompletionContext> GetCompletionContextAsync(IAsyncCompletionSession session, CompletionTrigger trigger, SnapshotPoint triggerLocation, SnapshotSpan applicableToSpan, CancellationToken token)
     {
         throw new NotImplementedException();
--- XamlCompletionSourceProvider.cs
+++ XamlCompletionSourceProvider.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright © 2026 Oleksandr Kukhtin. All rights reserved.
 
-using System;
 using System.Collections.Generic;
 using System.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Utilities;
 
 namespace A2v10XamlAutocomplete;
@@ -18,8 +15,19 @@ namespace A2v10XamlAutocomplete;
 [ContentType("xml")]
 internal class XamlCompletionSourceProvider : IAsyncCompletionSourceProvider
 {
+    private readonly Dictionary<ITextView, IAsyncCompletionSource> cache = new();
+
+    [Import]
+    ITextStructureNavigatorSelectorService StructureNavigatorSelector { get; set; }
+
     public IAsyncCompletionSource GetOrCreate(ITextView textView)
     {
-        throw new NotImplementedException();
+        if (cache.TryGetValue(textView, out var itemSource))
+            return itemSource;
+
+        var source = new XamlCompletionSource(StructureNavigatorSelector); // opportunity to pass in MEF parts
+        textView.Closed += (o, e) => cache.Remove(textView); // clean up memory as files are closed
+        cache.Add(textView, source);
+        return source;
     }
 }
